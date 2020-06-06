3d2fdbfcc24c52f9d0a357068d2b5aa6f787af0d
anita-sharma <2016uce1171@mnit.ac.in>
2020-06-06 19:17:40 +0530

    Updates

---
+++ precache-manifest.a9ea9ae6dc03e782f40df0ac2d557041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6551cbaa5d51db731457c14f85b8caa",
+    "url": "/react-github-api-project/index.html"
+  },
+  {
+    "revision": "f5bbfba5debff923292d",
+    "url": "/react-github-api-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b5af3892c12b71e50e9",
+    "url": "/react-github-api-project/static/js/2.630a1121.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-api-project/static/js/2.630a1121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bbfba5debff923292d",
+    "url": "/react-github-api-project/static/js/main.13e7c86b.chunk.js"
+  },
+  {
+    "revision": "0bfdbc66900d9439b634",
+    "url": "/react-github-api-project/static/js/runtime-main.6e154731.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-api-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
